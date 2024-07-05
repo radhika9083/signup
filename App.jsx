7364f2a99edfca6566e18489b24990eccd8de5e1
radhikagokhale <radhikagokhale99@gmail.com>
2024-07-05 11:00:48 +0530

    files added

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import New from "../src/New";
+function App() {
+
+  return (
+    <>
+    <New/>
+    </>
+  );
+}
+
+export default App
---
+++ New.jsx
@@ -0,0 +1,14 @@
+
+
+function New() {
+  return (
+    <div>
+        <div>
+            <h1>Login</h1>
+            <form action=""></form>
+        </div>
+    </div>
+  )
+}
+
+export default New;
